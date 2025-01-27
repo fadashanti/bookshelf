9fd85eec7f45f387f7d177a561b7a916326099ee
fadashanti <fadashanti09@gmail.com>
2025-01-28 00:31:34 +0100

    First Commit on BOOKSHELF

---
+++ components/BookList.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+const BookList = () => {
+  return (
+    <section className=''>
+      <h2 className="font-bebas-neue text-4xl text-light-100">Popular Books</h2>
+    </section>
+  );
+}
+ 
+export default BookList;
